6bce2385da0528286690c1809f11e3bce42cffc2
João Carlos <joaocarlos.machado05@gmail.com>
2026-05-09 14:56:43 -0300

    more bugs fixed

--- src/main/java/com/PedidoFeito/database/02_import_data_exemplo.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.produtos (
-                                 code     INTEGER      NOT NULL,
-                                 name     VARCHAR(200) NOT NULL,
-                                 unit     VARCHAR(10)  NOT NULL,
-                                 price    NUMERIC(10,2) NOT NULL,
-                                 category VARCHAR(50)  NOT NULL,
-                                 page     INTEGER      NOT NULL,
-                                 CONSTRAINT pk_produtos PRIMARY KEY (code)
-);
--- src/main/java/com/PedidoFeito/database/03_queries_uteis.sql
+++
@@ -1,5 +0,0 @@
-COPY public.produtos (code, name, unit, price, category, page)
-    FROM '/caminho/absoluto/produtos.csv'
-    DELIMITER ','
-    CSV HEADER
-    ENCODING 'UTF8';
